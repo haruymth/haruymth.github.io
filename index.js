7487fd64f5abd1cb09a20fe9fb37cb67f81f542c
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-07-23 08:14:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-function getAge(year, month, day){
-  // 生年月日
-  var birthday  = new Date(y, m-1, d);
-  // 今日の日付を取得
-  var today = new Date();
-  // 今年の誕生日を計算
-  var thisYearBirthday = new Date(today.getFullYear(), birthday.getMonth(), birthday.getDate());  
-  
-  // 今年から生年月日の年を引く
-  var age = today.getFullYear() - birthday.getFullYear();
-  
-  //今年の誕生日を迎えていなければ age-1 を返す
-  return (today < thisYearBirthday) ? age-1 : age;
+//生年月日
+const birthday = {
+    year: 2009,
+    month: 3,
+    date: 1
+  };
+
+function getAge(birthday){
+
+    //今日
+    var today = new Date();
+ 
+    //今年の誕生日
+    var thisYearsBirthday = new Date(today.getFullYear(), birthday.month-1, birthday.date);
+ 
+    //年齢
+    var age = today.getFullYear() - birthday.year;
+
+    if(today < thisYearsBirthday){
+        //今年まだ誕生日が来ていない
+        age--;
+    }
+
+    return age;
 }
 
-var myAge = function getAge(2009,3,1);
-console.log(myAge);
-document.getElementById("age").innerText = "日本在住"+myAge+"才";
+console.log(getAge(birthday));
+
+document.getElementById("age").innerText=getAge(birthday);
