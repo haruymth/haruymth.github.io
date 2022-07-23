6272848b83e1c7eed1eaaec98deffb4a07369474
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-07-23 09:50:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const birthdate = "2009/3/1";
- 
-const ageCalculation = ( birthDate , nowDate ) => {
-    const age = nowDate.getFullYear() - birthDate.getFullYear();
-    const thisYearsBirthday  = new Date( nowDate.getFullYear() , birthDate.getMonth() , birthDate.getDate() );
-    return age + ( thisYearsBirthday.getTime() > nowDate.getTime() ? -1 : 0 );
-}
- 
-const age = ageCalculation( new Date(birthdate) , new Date() );
- 
-console.log( age + "歳" );
- 
-document.getElementById("age").innerText="・日本在住"+age+"歳";
+
