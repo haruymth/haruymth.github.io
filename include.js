076a4b8d0c742123a152666241b0a02ca08e9e7c
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-09-19 10:50:30 +0900

    Update include.js

--- include.js
+++ include.js
@@ -1,4 +1,4 @@
-var header = getElementsByTagName("header");
+var header = document.getElementsByTagName("header");
 header.innerHTML = (`
 	<header style="width:100%;height:65px;background-color:#51BEBE;box-shadow: 0px 2px 5px 1px rgba(0, 0, 0, 0.3);margin:0px;position: fixed;top: 0;left: 0;">
 		<ul>
@@ -7,7 +7,7 @@ header.innerHTML = (`
 				<li> <a href="https://haru-ymth.github.io/others" class="nav">その他</a> </li>
 		</ul>
 	</header>`);
-var footer = getElementsByTagName("footer");
+var footer = document.getElementsByTagName("footer");
 footer.innerHTML = (`
 	<footer style="text-align:center; background-color:#233648;color:#ffffff;margin-top:60px;padding:5px;bottom:0;">
 		© 2022 yamaguchi03
