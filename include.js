0f2a159682fd42cb13a25ae52410c3a710eaa42b
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-09-19 14:48:54 +0900

    Update include.js

--- include.js
+++ include.js
@@ -1,7 +1,5 @@
 var header = document.getElementById("header");
 header.innerHTML = `
-<link rel="apple-touch-icon" sizes="180x180" href="https://haru-ymth.github.io/images/7TKsOLDBCLSjgKf1663558204_1663558208.png">
-<link rel="icon" type="image/x-icon" href="https://haru-ymth.github.io/images/favicon.ico">
 	<header style="width:100%;height:65px;background-color:#51BEBE;box-shadow: 0px 2px 5px 1px rgba(0, 0, 0, 0.3);margin:0px;position: fixed;top: 0;left: 0;">
 		<ul>
 				<li> <a href="https://haru-ymth.github.io/" class="nav">yamaguchi03のホームページ</a> </li>
