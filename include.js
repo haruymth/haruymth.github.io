60c0a9931b8a7804621fcdf51ab1d82c6caf2774
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-09-20 18:01:27 +0900

    Update include.js

--- include.js
+++ include.js
@@ -2,7 +2,8 @@ var header = document.getElementById("header");
 header.innerHTML = `
 	<header>
 		<ul>
-				<li> <a href="https://haru-ymth.github.io/" class="nav"><img src="https://haru-ymth.github.io/favicon.png" width="40px" height="40px" class="nav">yamaguchi03</a> </li>
+				<li> <a href="https://haru-ymth.github.io/" class="nav"><img src="https://haru-ymth.github.io/favicon.png" width="40px" height="40px" class="nav"></a> </li>
+				<li><a href="https://haru-ymth.github.io/" class="nav">yamaguchi03</a></li>
 				<li> <a href="https://haru-ymth.github.io/others" class="nav">その他</a> </li>
 		</ul>
 	</header>`;
