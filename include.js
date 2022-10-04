f49a36471de5e540b14bdcf115579cdc9526aec2
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-04 17:39:05 +0900

    Update include.js

--- include.js
+++ include.js
@@ -1,18 +1,18 @@
-//var header = document.getElementById("header");
-//header.innerHTML = `
-//	<header>
-//		<ul>
-//				<li> <a href="https://haru-ymth.github.io/" class="nav"><img src="https://haru-ymth.github.io/favicon.png" width="35px" height="35px" id="icon"></a> </li>
-//				<li><a href="https://haru-ymth.github.io/" class="nav">yamaguchi03</a></li>
-//				<li> <a href="https://haru-ymth.github.io/others" class="nav">その他</a> </li>
-//		</ul>
-//	</header>`;
-var head =document.getElementsByTagName("head")[0];
-var header = document.createElement("header");
-header.innerHTML =`
-<ul>
-	<li> <a href="https://haru-ymth.github.io/" class="nav"><img src="https://haru-ymth.github.io/favicon.png" width="35px" height="35px" id="icon"></a> </li>
-	<li><a href="https://haru-ymth.github.io/" class="nav">yamaguchi03</a></li>
-	<li> <a href="https://haru-ymth.github.io/others" class="nav">その他</a> </li>
-</ul>`;
-head.after(header);
+var header = document.getElementById("header");
+header.innerHTML = `
+	<header>
+		<ul>
+				<li> <a href="https://haru-ymth.github.io/" class="nav"><img src="https://haru-ymth.github.io/favicon.png" width="35px" height="35px" id="icon"></a> </li>
+				<li><a href="https://haru-ymth.github.io/" class="nav">yamaguchi03</a></li>
+				<li> <a href="https://haru-ymth.github.io/others" class="nav">その他</a> </li>
+		</ul>
+	</header>`;
+//var head =document.getElementsByTagName("head")[0];
+//var header = document.createElement("header");
+//header.innerHTML =`
+//<ul>
+//	<li> <a href="https://haru-ymth.github.io/" class="nav"><img src="https://haru-ymth.github.io/favicon.png" width="35px" height="35px" id="icon"></a> </li>
+//	<li><a href="https://haru-ymth.github.io/" class="nav">yamaguchi03</a></li>
+//	<li> <a href="https://haru-ymth.github.io/others" class="nav">その他</a> </li>
+//</ul>`;
+//head.after(header);
