2a18bdd6b15f0d27295abfc049372ab4029869aa
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-05 07:57:51 +0900

    Update include.js

--- include.js
+++ include.js
@@ -7,16 +7,9 @@ header.innerHTML = `
 		</ul>`;
 var footer=document.createElement("footer");
 footer.innerHTML=`
-&copy;&nbsp;2022&nbsp;yamaguchi03`;
-var head=getElementsByTagName("head")[0];
-document.body.insertBefore(header,head);
-document.bodu.appendChild(footer);
-//var head =document.getElementsByTagName("head")[0];
-//var header = document.createElement("header");
-//header.innerHTML =`
-//<ul>
-//	<li> <a href="https://haru-ymth.github.io/" class="nav"><img src="https://haru-ymth.github.io/favicon.png" width="35px" height="35px" id="icon"></a> </li>
-//	<li><a href="https://haru-ymth.github.io/" class="nav">yamaguchi03</a></li>
-//	<li> <a href="https://haru-ymth.github.io/others" class="nav">その他</a> </li>
-//</ul>`;
-//head.after(header);
+&copy;&nbsp;2022&nbsp;yamaguchi03
+`;
+var head=document.getElementsByTagName("head")[0];
+head.before(header);
+var body=document.getElementsByTagName("body")[0];
+body.after(footer);
