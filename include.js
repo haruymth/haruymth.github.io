0e7cc244cb04821e0c6da025bc25c4f355d936a7
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-08 12:26:39 +0900

    Update include.js

--- include.js
+++ include.js
@@ -1,3 +1,17 @@
+function getCookieValue(key){
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+        var cookiesArray = cookie.split('='); 
+        if (cookiesArray[0].trim() == key.trim()) { 
+            return cookiesArray[1];  // (key[0],value[1])
+        }
+    }
+    return '';
+}
+if(getCookieValue("haruymththeme")==""){
+document.cookie="haruymththeme=light";
+}
+let lightOrDark=getCookieValue("haruymththeme");
 window.onload=function(){var header = document.createElement("header");
 header.innerHTML = `
 		<ul id=headerul>
