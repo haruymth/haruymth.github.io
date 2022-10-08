730c02f51b0faf2710f8c97650a4c8dfa587ab55
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-08 16:41:12 +0900

    Update include.js

--- include.js
+++ include.js
@@ -1,6 +1,5 @@
 const head = document.head;
 head.insertAdjacentHTML('afterBegin', `	
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	  <link rel="stylesheet" href="https://haru-ymth.github.io/style.css">
 	  <meta charset="utf-8"> 
 	  <link rel="icon" href="https://haru-ymth.github.io/favicon.png">`);
@@ -56,88 +55,100 @@ if(getCookieValue("haruymththeme")==""){
 document.cookie="haruymththeme=light;path=/";
 }
 let lightOrDark=getCookieValue("haruymththeme");
-//
-			 window.addEventListener('DOMContentLoaded', function(){
 
-function themeSet(mode){
-	if(mode=="dark"){
-	$("body").css(
-		{"background-color":"#233648","color":"#f4f6f7"
-		});
-	$("h2").css(
-		{
-		"color":"#f4f6f7"
-		});
-	$("a").css(
-		{
-		"color":"#FFF7D6"
-		});
-	$("a:hover").css(
-		{
-		"color":"#FFF7D6"
-		});
-	$(".code").css(
-		{
-		"color":"#233648",
-		"background-color":"#f4f6f7"
-		});
-	lightOrDark="dark";
-		theme.innerHTML=`
-        <img src="/imgs/${lightOrDark}now.png" width="25" height="25">`;
-		document.cookie=`haruymththeme=${lightOrDark};path=/`;
-	}
-	if(mode=="light"){
-	$("body").css(
-		{"background-color":"#f4f6f7","color":"#233648"
-		});
-	$("h2").css(
-		{
-		"color":"#233648"
-		});
-	$("a").css(
-		{
-		"color":"#4b749c"
-		});
-	$("a:hover").css(
-		{
-		"color":"#4b749c"
-		});
-	$(".code").css(
-		{
-		"color":"#f4f6f7",
-		"background-color":"#233648"
-		});
-	lightOrDark="light";
-		theme.innerHTML=`
-        <img src="/imgs/${lightOrDark}now.png" width="25" height="25">`;
-		document.cookie=`haruymththeme=${lightOrDark};path=/`;
-	}
-	$("a.nav").css({
-  "text-decoration":"none",
-  "color":"#ffffff",
-  "text-align":"center",
-  "font-weight":"600",
-  "vertical-align": "middle",
-  "padding":"20px"
-});
-$("a.nav:hover").css({
-  "text-decoration":"none",
-  "color":"#ffffff",
-  "text-align":"center",
-  "font-weight":"600",
-  "vertical-align": "middle",
-  "padding":"20px",
-});
-}
-themeSet(lightOrDark);
-theme.onclick=function(){
-	if(lightOrDark=="light"){
-	themeSet("dark");
-	}else{
-	themeSet("light");
-	}
-	
-};
-//
+			 window.addEventListener('DOMContentLoaded', function(){
+        var head = document.getElementsByTagName('head')
+        var script = document.createElement('script')
+        //ここで任意のjQueryを読み込ませる
+        script.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js')
+        script.setAttribute('type', 'text/javascript')
+        script.addEventListener('load', function() {
+        
+          // ここにjQueryの記述をする
+          $(function() {
+            function themeSet(mode){
+              if(mode=="dark"){
+              $("body").css(
+                {"background-color":"#233648","color":"#f4f6f7"
+                });
+              $("h2").css(
+                {
+                "color":"#f4f6f7"
+                });
+              $("a").css(
+                {
+                "color":"#FFF7D6"
+                });
+              $("a:hover").css(
+                {
+                "color":"#FFF7D6"
+                });
+              $(".code").css(
+                {
+                "color":"#233648",
+                "background-color":"#f4f6f7"
+                });
+              lightOrDark="dark";
+                theme.innerHTML=`
+                    <img src="/imgs/${lightOrDark}now.png" width="25" height="25">`;
+                document.cookie=`haruymththeme=${lightOrDark};path=/`;
+              }
+              if(mode=="light"){
+              $("body").css(
+                {"background-color":"#f4f6f7","color":"#233648"
+                });
+              $("h2").css(
+                {
+                "color":"#233648"
+                });
+              $("a").css(
+                {
+                "color":"#4b749c"
+                });
+              $("a:hover").css(
+                {
+                "color":"#4b749c"
+                });
+              $(".code").css(
+                {
+                "color":"#f4f6f7",
+                "background-color":"#233648"
+                });
+              lightOrDark="light";
+                theme.innerHTML=`
+                    <img src="/imgs/${lightOrDark}now.png" width="25" height="25">`;
+                document.cookie=`haruymththeme=${lightOrDark};path=/`;
+              }
+              $("a.nav").css({
+              "text-decoration":"none",
+              "color":"#ffffff",
+              "text-align":"center",
+              "font-weight":"600",
+              "vertical-align": "middle",
+              "padding":"20px"
+            });
+            $("a.nav:hover").css({
+              "text-decoration":"none",
+              "color":"#ffffff",
+              "text-align":"center",
+              "font-weight":"600",
+              "vertical-align": "middle",
+              "padding":"20px",
+            });
+            }
+            themeSet(lightOrDark);
+            theme.onclick=function(){
+              if(lightOrDark=="light"){
+              themeSet("dark");
+              }else{
+              themeSet("light");
+              }
+              
+            };
+          })
+        
+        })
+        document.head.appendChild(script)
+
 			 });
 };
