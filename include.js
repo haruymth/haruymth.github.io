9815209d17ac72ea3240d47973c0e248955fc77b
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-08 16:30:01 +0900

    Update include.js

--- include.js
+++ include.js
@@ -1,8 +1,6 @@
 const head = document.head;
-head.insertAdjacentHTML('afterBegin', `	  <script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"
-  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-  crossorigin="anonymous"></script>
+head.insertAdjacentHTML('afterBegin', `	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	  <link rel="stylesheet" href="https://haru-ymth.github.io/style.css">
 	  <meta charset="utf-8"> 
 	  <link rel="icon" href="https://haru-ymth.github.io/favicon.png">`);
