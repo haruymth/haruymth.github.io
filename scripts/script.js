beeace723682acd90ede7fb7fb46e61b55c21018
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-10 08:02:03 +0900

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+var code=document.getElementsByClassName("code");
+for(let i=0;i<code.length;i++){
+	var button=document.createElement("button");
+  button.setAttribute("class","button");
+  button.setAttribute("onclick","copyScripts(this.previousElementSibling);");
+  button.innerHTML="Copy";
+	code[i].after(button);
+};
+function copyScripts(scripts){
+	var element = document.createElement("textarea");
+	element.value=scripts.textContent;
+	document.body.appendChild(element);
+	element.select();
+	document.execCommand('copy');
+	document.body.removeChild(element)
+	scripts.nextElementSibling.innerHTML="Copied!";
+	setTimeout(function(){scripts.nextElementSibling.innerHTML="Copy"},1000);
+};
