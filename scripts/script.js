1f7d103e323a20db5efaa27a7bfd9d3f3d00d905
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-14 18:27:20 +0900

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -20,12 +20,15 @@ for(let i=0;i<code.length;i++){
 };*/
 function copyScripts(scripts){
 	var element = document.createElement("textarea");
-    	var code=(await(await fetch(`/scripts/codes/${scripts.id}.txt`,{headers:{"X-Requested-With":"XMLHttpRequest"}})).text());
-	element.value=code;
+  fetch(`https://haru-ymth.github.io/scripts/codes/${scripts.id}.txt`)
+  .then(response => response.text())
+  .then(data => {
+	element.value=data;
 	document.body.appendChild(element);
 	element.select();
 	document.execCommand('copy');
 	document.body.removeChild(element);
 	scripts.nextElementSibling.innerHTML="Copied!";
 	setTimeout(function(){scripts.nextElementSibling.innerHTML="Copy"},1000);
+  });
 };
