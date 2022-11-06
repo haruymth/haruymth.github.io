42e1a100ff4bcac2f24498af1dc4dad5b8755863
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-11-06 20:16:37 +0900

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ for(let i=0;i<code.length;i++){
   var div=document.createElement("div");
   div.style="display: flex;";
     div.innerHTML=`
-  <button class="button" onclick="copyScripts(this.previousElementSibling);">Copy</button>
+  <button class="button" onclick="copyScripts(this.parentNode.previousElementSibling);">Copy</button>
   <main style="margin:auto 0 0 0;padding:7px;font-size:15px;opacity:0;transition: opacity 0.2s;">Copied!</main>
   `;
 	code[i].after(div);
@@ -21,7 +21,7 @@ function copyScripts(scripts){
 	element.select();
 	document.execCommand('copy');
 	document.body.removeChild(element);
-	scripts.nextElementSibling.style.opacity="0.7";
-	setTimeout(function(){scripts.nextElementSibling.style.opacity="0"},3000);
+	scripts.nextElementSibling.children[0].style.opacity="0.7";
+	setTimeout(function(){scripts.nextElementSibling.children[0].style.opacity="0"},3000);
   });
 };
