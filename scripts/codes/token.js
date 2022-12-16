7e7805693d199ee07fe9272fe33f632823f40c7f
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-16 15:58:26 +0900

    Delete token.js

--- scripts/codes/token.js
+++
@@ -1,5 +0,0 @@
-(async()=>{
-  if(!location.hostname.includes("scratch.mit.edu")){return;}
-  let token=(await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
-  let username=(await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.username;
-})();
