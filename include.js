b14cf878a0d24f2c4d4e14257239f57162684670
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-25 11:27:46 +0900

    Update include.js

--- include.js
+++ include.js
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', async function() {
 	document.body.after(footer)
     var menu=document.createElement("iframe");
     menu.id="menubar";
-    menu.src="https://haruymth.github.io/others";
+    menu.src="/others";
     menu.class="none";
     var head=document.head;
     head.after(header);
