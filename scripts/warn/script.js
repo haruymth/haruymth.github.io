f5f70cd1dd77fefa67e9157045783e642f6ae41b
haruymth <108157117+haruymth@users.noreply.github.com>
2023-02-05 18:16:30 +0900

    Update script.js

--- scripts/warn/script.js
+++ scripts/warn/script.js
@@ -12,15 +12,20 @@ function getCookieValue(key) {
 }
 	
 	
-function copyScripts(scripts){
+async function copyScripts(scripts){
 	var element = document.createElement("textarea");
-  fetch(`https://thscripts.haru-ymth.repl.co/scripts/${scripts.id}?password=${getCookieValue("password")}`)
-  .then(response => response.text())
-  .then(data => {
-	navigator.clipboard.writeText(data);
+  const res=await fetch(`https://thscripts.haru-ymth.repl.co/scripts/${scripts.id}?password=${getCookieValue("password")}`);
+	const key=res.headers.get("x-token")
+	function crypto2(src,key) {
+		var des = "";
+		var decrypted = CryptoJS.AES.decrypt(src, key);
+		des = decrypted.toString(CryptoJS.enc.Utf8);
+		return des
+	}
+	const src=await res.text();
+	navigator.clipboard.writeText(crypto2(src,key));
 	scripts.nextElementSibling.children[1].style.opacity="0.7";
 	setTimeout(function(){scripts.nextElementSibling.children[1].style.opacity="0"},3000);
-  });
 };
 }catch(e){
 	alert(e)
