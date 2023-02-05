43ce274edef26d54dc4c304179d2e0800d50aca6
haruymth <108157117+haruymth@users.noreply.github.com>
2023-02-05 16:25:59 +0900

    Update script.js

--- scripts/warn/script.js
+++ scripts/warn/script.js
@@ -11,16 +11,7 @@ function getCookieValue(key) {
     return '';
 }
 	
-	var code=document.getElementsByClassName("script");
-for(let i=0;i<code.length;i++){
-  var div=document.createElement("div");
-  div.style="display: flex;";
-    div.innerHTML=`
-  <button class="button" onclick="copyScripts(this.parentNode.previousElementSibling);">Copy</button>
-  <main style="margin:auto 0 0 0;padding:7px;font-size:15px;opacity:0;transition: opacity 0.2s;">Copied!</main>
-  `;
-	code[i].after(div);
-};
+	
 function copyScripts(scripts){
 	var element = document.createElement("textarea");
   fetch(`https://thscripts.haru-ymth.repl.co/scripts/${scripts.id}?password=${getCookieValue("password")}`)
